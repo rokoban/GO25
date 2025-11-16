405c84007eff45bf4fedc3de309f31df84ced1f6
ろこ大蛇 <rokoban@proton.me>
2025-11-16 02:11:02 +0000

    feat(ui): basic menu and scene switch setup

--- res/ui/main_menu.gd
+++ res/ui/main_menu.gd
@@ -1,4 +1,14 @@
 extends Control
 
 func _ready() -> void:
-	print("やほ〜")
+	pass
+
+func start_game():
+	get_tree().change_scene_to_file("res://levels/game_root.tscn")
+
+func quit_game():
+	get_tree().quit()
+
+
+func _on_new_game_button_pressed() -> void: start_game()
+func _on_exit_button_pressed() -> void: quit_game()
